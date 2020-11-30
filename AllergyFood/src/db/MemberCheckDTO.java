07fe213e6db5577747858e5211404454e780fd0d
Green_Computer <Green_Computer@DESKTOP-912HOCH>
2020-11-30 09:52:27 +0900

    test

---
+++ AllergyFood/src/db/MemberCheckDTO.java
@@ -0,0 +1,42 @@
+package db;
+
+public class MemberCheckDTO {
+	private String member_id;
+	private String food_name;
+	private String food_factory;
+	
+	public MemberCheckDTO() {
+		
+	}
+	
+	public MemberCheckDTO(String member_id, String food_name, String food_factory) {
+		this.member_id = member_id;
+		this.food_name = food_name;
+		this.food_factory  = food_factory;
+	}
+	
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getFood_name() {
+		return food_name;
+	}
+	public void setFood_name(String food_name) {
+		this.food_name = food_name;
+	}
+	public String getFood_factory() {
+		return food_factory;
+	}
+	public void setFood_factory(String food_factory) {
+		this.food_factory = food_factory;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberFoodDTO [member_id=" + member_id + ", food_name=" + food_name + ", food_factory=" + food_factory
+				+ "]";
+	}
+}
